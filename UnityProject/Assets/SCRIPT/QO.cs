0c2dc0f6420dc3cefddfac2e12690f49b97611c6
a4608635 <57396749+a4608635@users.noreply.github.com>
2019-11-26 19:26:50 +0800

    完成

---
+++ UnityProject/Assets/SCRIPT/QO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QO : MonoBehaviour
+{
+    private void Start()
+    {
+        print("遊戲開始");
+    }
+
+    private void Update()
+    {
+        print("咕咕咕!");
+    }
+}
